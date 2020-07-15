910de3f7df217769c3efb075a7dd163804232ed5
tom <tommy.nguyen0721@gmail.com>
2020-07-15 10:53:04 -0700

    Add GroceryList

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+var GroceryList = (props) => (
+    <ul>
+        <li>{props.grocery[0]}</li>
+        <li>{props.grocery[1]}</li>
+    </ul>
+);
+
+var App = () => (
+    <div>
+        <h2>Grocery List</h2>
+        <GroceryList grocery={['kale', 'cucumber']} />
+    </div>
+);
+ReactDOM.render(<App />, document.getElementById('app'));
+
+
