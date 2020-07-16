583553ed6a9ca4a77f5cc72d55e25786b7f5d679
tom <tommy.nguyen0721@gmail.com>
2020-07-15 18:01:03 -0700

    Create GroceryListItem class

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
-var GroceryList = (props) => (    
-    <GroceryListItem groceryItems={['kale', 'cucumber', 'lettuce', 'tomatoe']}/>
-);
+// Create an <ul> component called GroceryList
+// render (using ReactDom.render) to append GroceryList component to div#app
+// Create two new <li> components Cucumbers, and Kale 
 
-var GroceryListItem = (props) => (
+// Interact with the clicked item to show it's been selected
+var GroceryList = (props) => (
     <ul>
-        {props.groceryItems.map((grocery, i) => (
-            <li key={i}>{grocery}</li>
-        ))}
+        {props.items.map( (item) =>
+            <GroceryListItem item={item} /> 
+        )}
     </ul>
 );
-
-var App = () => (
-    <div>
-        <h2>Grocery List</h2>
-        <GroceryList  />
-    </div>
+var Kale = () => (
+    <li>
+        kale
+    </li>
 );
-ReactDOM.render(<App />, document.getElementById('app'));
+class GroceryListItem extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hover: false
+        }
+    }
+    onHover() {
+        this.setState({
+            hover: !this.state.hover
+        });
+
+    }
+    render() {
+        var style = {
+            fontWeight: this.state.hover ? 'bold' : 'normal'
+        }
+        return (
+            <li style={style} onMouseOver={this.onHover.bind(this)}>{this.props.item}</li>
+        );
+    }
+}
 
 
+ReactDOM.render(<GroceryList items={['kale', 'cucumber']} />, document.getElementById('app'));
+
+
+
+
+
+/* 
+var App = () => (
+<div>Some cliche salutation</div>
+);
+*/
